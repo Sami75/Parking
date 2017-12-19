4541dff8ea63a236393679ce2258553a3aac10e6
Sami75 <sami.thairi@hotmail.fr>
2017-12-19 01:09:33 +0100

    Reglages

--- laravel/app/Http/Controllers/Contacts.php
+++ laravel/app/Http/Controllers/Contacts.php
@@ -15,8 +15,15 @@ class Contacts extends Controller
 
     public function store(Request $request)
     {
-    	Mail::to('sami.thairi@hotmail.fr')
-    		->send(new Contact($request->except('_token')));
+    	$login = $request->input('login');
+    	$email = $request->input('email');
+
+    	Mail::send('auth.mdpoublier', ['login' => $login, 'email' => $email], function($message)
+    	{
+    			$message->from($email);
+
+    			$message->to('sami.thair@hotmail.fr');
+    	});
 
     	echo'<script>alert("Votre demande de nouveau mot de passe a était envoyé à l\'administrateur, il vous enverra un mail avec un nouveau mot de passe !"); window.location.href="/"</script>';
     }
